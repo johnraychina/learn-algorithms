a7c7c148502deb168073f1d20e45153fb88f7c46
reed.zy <reed.zy@alibaba-inc.com>
2020-08-06 10:22:04 +0800

    work work

---
+++ graph/src/main/java/com/zhangyi/graph/Cycle.java
@@ -0,0 +1,52 @@
+package com.zhangyi.graph;
+
+import algs4.Graph;
+import algs4.Stack;
+
+/**
+ * 无环图和树等价，dfs往下遍历子节点会构成一棵树，所有节点应该都只被遍历一次，若是发现有子节点已经被标记（遍历）过，就说明构成环。
+ *
+ * @author 张义 johnraychina@163.com
+ */
+public class Cycle {
+
+    private boolean[] marked;
+    private Stack<Integer> cycle;
+    private boolean hasCycle;
+
+    public Cycle(Graph g, int s) {
+        //initialize data structures
+        marked = new boolean[g.V()];
+        hasCycle = false;
+
+        dfs(g, s, s);
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * adj(u) = { v, v1, v2 ... } adj(v) = { w==u, w1, w2, w3 ... }
+     *
+     * @param G graph
+     * @param v current node
+     * @param u parent node
+     */
+    private void dfs(Graph G, int v, int u) {
+        mark(v);
+
+        for (int w : G.adj(v)) {
+            if (!marked[w]) {
+                dfs(G, w, v);
+            } else if (w != u) {
+                hasCycle = true;
+            }
+        }
+    }
+
+    private void mark(int v) {
+        marked[v] = true;
+        System.out.println(v);
+    }
+}
---
+++ graph/src/main/java/com/zhangyi/graph/TwoColor.java
@@ -0,0 +1,50 @@
+package com.zhangyi.graph;
+
+import algs4.Graph;
+import algs4.Stack;
+
+/**
+ * 给图的所有顶点涂色，两个直连的顶点颜色必须不同，能否只用2个颜色？ 无环图一定可以实现2个颜色着色，只要判断有环，且 已标记的顶点颜色 == 当前顶点颜色，则无法满足着色条件。
+ *
+ * @author 张义 johnraychina@163.com
+ */
+public class TwoColor {
+
+    private boolean[] marked;
+    private boolean[] color;
+    private boolean isTwoColor;
+
+    public TwoColor(Graph g, int s) {
+        //initialize data structures
+        marked = new boolean[g.V()];
+        color = new boolean[g.V()];
+        isTwoColor = true;
+
+        dfs(g, s, s);
+    }
+
+    /**
+     * adj(u) = { v, v1, v2 ... } adj(v) = { w==u, w1, w2, w3 ... }
+     *
+     * @param G graph
+     * @param v current node
+     * @param u parent node
+     */
+    private void dfs(Graph G, int v, int u) {
+        mark(v);
+
+        for (int w : G.adj(v)) {
+            if (!marked[w]) {
+                dfs(G, w, v);
+            } else if (w != u) {
+                isTwoColor = !(color[w] == color[v]);
+            }
+        }
+    }
+
+    private void mark(int v) {
+        marked[v] = true;
+        System.out.println(v);
+    }
+
+}
